6beefc58fbb433ac4687f10790d2c214a6cbb9c2
Damien Nelson <dhnisaman@gmail.com>
2024-09-29 17:02:50 -0400

    add astroid and astroid feild render update main to render to screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame 
 from constants import * 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 
@@ -11,20 +13,28 @@ def main():
   game = True
   x = SCREEN_WIDTH / 2
   y = SCREEN_HEIGHT / 2
-  player = Player(x, y)
-  updatable = pygame.sprite.Group(player)
-  drawable = pygame.sprite.Group(player)
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
 
+  Player.containers = (updatable, drawable)
+  AsteroidField.containers = (updatable)
+  Asteroid.containers = (asteroids, updatable, drawable)
+
+  feild = AsteroidField()
+
+  player = Player(x, y)
+  
   while game:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return
     screen.fill(000)
-    for draw in drawable:
-      draw.draw(screen)
     for update in updatable:
       update.update(dt)
-      
+    for draw in drawable:
+      draw.draw(screen)
+    
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
