4a40430f189223c340d9fd1626928dbcedd9702d
Damien Nelson <dhnisaman@gmail.com>
2024-09-29 20:29:05 -0400

    add shot class update player shoot method enable shot on space and render

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 
@@ -16,10 +17,12 @@ def main():
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
 
   Player.containers = (updatable, drawable)
   AsteroidField.containers = (updatable)
   Asteroid.containers = (asteroids, updatable, drawable)
+  Shot.containers = (shots, updatable, drawable)
 
   feild = AsteroidField()
 
@@ -34,6 +37,11 @@ def main():
       update.update(dt)
     for draw in drawable:
       draw.draw(screen)
+    for asteroid in asteroids:
+      if asteroid.check_collision(player):
+        print("Game over!")
+        game = False
+        
     
     pygame.display.flip()
     dt = clock.tick(60) / 1000
